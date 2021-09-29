97681c4beef736150009fa0b106bdc9eb2afa42f
Joyvanoostveen <90258878+Joyvanoostveen@users.noreply.github.com>
2021-09-29 22:28:36 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
 // JavaScript Document
+
+// var button = document.getElementsByClassName("button");
+// var upcoming = document.getElementsByClassName("upcoming");
+
+// function openUpcoming() {
+//     upcoming.classList.add("toonUpcoming")
+// }
+// button.addEventListener("click", openUpcoming);
+
+
+
+// var deButton = document.querySelector("h2");
+
+// function doeFormNeerEnOp(){
+// 	let hetFormulier = document.querySelector("form");
+// 	hetFormulier.classList.toggle("toonForm");
+// }
+
+// deButton.addEventListener("click", doeFormNeerEnOp);
+
+
+var button = document.getElementsByClassName("button");
+let upcoming = document.querySelector("section:last-of-type");
+
+function openUpcoming() {
+    upcoming.classlist.toggle("toonUpcoming");
+}
+
+button.addEventListener("openUpcoming");
